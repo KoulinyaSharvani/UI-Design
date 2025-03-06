1864791f4d7d53653205e1e1133d557d7176d880
KoulinyaSharvani <sharvani.koulinya@gmail.com>
2025-03-06 10:06:22 +0530

    Add files via upload

---
+++ Second.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+void main(){
+	print("ACE");
+	print("College");
+	stdout.write("hi");
+	stdout.write("hi");
+
+}
---
+++ Third.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+void main(){
+	print("ACE");
+	print("College");
+	stdin.readLineSync()!;
+	stdout.write("hi");
+	stdout.write("hi");
+
+}
